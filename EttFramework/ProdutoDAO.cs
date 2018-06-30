b3f6383132144be0ddf1b70baf8ef4d0d310554d
Luis Fernando <luis.city@gmail.com>
2018-06-30 02:32:20 -0300

    classe que representa a entidade e classe DAO com metodo adicionar

--- EttFramework/ProdutoDAO.cs
+++ EttFramework/ProdutoDAO.cs
@@ -9,8 +9,29 @@ namespace ADONet
 
         public ProdutoDAO()
         {
-            this.conexao.ConnectionString = "Data Source=DESKTOP-K6073DO;Initial Catalog=HondaFitParts;Persist Security Info=True;User ID=sa;Password=asdasd";            
+            this.conexao = new SqlConnection("Data Source=DESKTOP-K6073DO;Initial Catalog=HondaFitParts;Persist Security Info=True;User ID=sa;Password=asdasd");
             this.conexao.Open();
+        }
+
+        public void Dispose()
+        {
+            this.conexao.Close();
+        }
+
+        internal void Adicionar(Produto produto) {
+            IDbCommand cmd = conexao.CreateCommand();
+            cmd.CommandText = @"INSERT INTO [dbo].[Produto] ([Nome],[Descricao],[Imagem]) VALUES (@Nome ,@Descricao ,@Imagem)";
+
+            IDbDataParameter paranNome = new SqlParameter("Nome", produto.Nome);
+            cmd.Parameters.Add(paranNome);
+
+            IDbDataParameter paranDescricao = new SqlParameter("Descricao", produto.Descricao);
+            cmd.Parameters.Add(paranDescricao);
+
+            IDbDataParameter paranImagem = new SqlParameter("Imagem", produto.Imagem);
+            cmd.Parameters.Add(paranImagem);
+
+            cmd.ExecuteNonQuery();
         }
     }
 }
