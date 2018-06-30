f7904943461a371915edccc25db30417d1fca89f
Luis Fernando <luis.city@gmail.com>
2018-06-30 02:01:46 -0300

    Criado classe disposable de acesso a dados, e abrindo a coneção no construtor com ajuda da interface IDbConnection

---
+++ EttFramework/ProdutoDAO.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ADONet
+{
+    class ProdutoDAO: System.IDisposable
+    {
+        private IDbConnection conexao;
+
+        public ProdutoDAO()
+        {
+            this.conexao.ConnectionString = "Data Source=DESKTOP-K6073DO;Initial Catalog=HondaFitParts;Persist Security Info=True;User ID=sa;Password=asdasd";            
+            this.conexao.Open();
+        }
+    }
+}
