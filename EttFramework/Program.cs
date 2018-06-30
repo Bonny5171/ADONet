e20882aa5f8b86a7eef54fc66ef6235f74a19078
Luis Fernando <luis.city@gmail.com>
2018-06-30 02:32:39 -0300

    arquivos do visual studio

--- EttFramework/Program.cs
+++ EttFramework/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EttFramework
+namespace ADONet
 {
     class Program
     {
         static void Main(string[] args)
         {
+            GravarProdutoNoBanco();
+        }
+
+        private static void GravarProdutoNoBanco()
+        {
+            ProdutoDAO produtoDAO = new ProdutoDAO();
+
+            produtoDAO.Adicionar(new Produto()
+            {
+                Nome = "Lantera",
+                Descricao = "Luz xenom extra forte.",
+                Imagem = "https://http2.mlstatic.com/D_Q_NP_235121-MLB20705073330_052016-Q.jpg"
+            });
+
         }
     }
 }
